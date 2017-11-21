8d4d48511ece26b23c75a303686bc2bb7db7cca9
sroya <shekeb.roya@hsr.ch>
2017-11-21 20:02:24 +0100

    google userid saved in mongodb

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new googleStrategy(
@@ -10,10 +13,11 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refeshToken, profile, done) => {
-      console.log('accessToken: ', accessToken);
-      console.log('refeshToken: ', refeshToken);
-      console.log('profile: ', profile);
-      console.log('done: ', done);
+      //console.log('accessToken: ', accessToken);
+      //console.log('refeshToken: ', refeshToken);
+      //console.log('profile: ', profile);
+      //console.log('done: ', done);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
