94d21e9f247b455b7360bcf79ec9153b435993cb
sroya <shekeb.roya@hsr.ch>
2017-11-22 02:36:33 +0100

    test

--- services/passport.js
+++ services/passport.js
@@ -19,8 +19,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true
+      callbackURL: '/auth/google/callback'
     },
     (accessToken, refeshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
