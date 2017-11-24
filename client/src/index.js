5f8ce6cc57ea2272db2f13991e5cb128c42e9a2c
sroya <shekeb.roya@hsr.ch>
2017-11-25 00:13:35 +0100

    reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(<App />, document.querySelector('#root'));
