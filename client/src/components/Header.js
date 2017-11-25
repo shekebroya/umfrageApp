d04bf977bbb82ddd2c3f86b96484929ba111c456
sroya <shekeb.roya@hsr.ch>
2017-11-25 18:42:40 +0100

    preprocessor + materializer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import './Header.css'; // Tell Webpack that Header.js uses these styles
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h3>Header</h3>
+      <div className="header">
+        <div className="container">
+          <div className="row">
+            <div className="header__logo col s4">
+              <a className="header__logo__link" href="/">
+                UM<i className="medium">frage</i>
+                <i className="small">?!</i>
+              </a>
+            </div>
+            <ul className="header__navigation col s8">
+              <li>Login to google</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
