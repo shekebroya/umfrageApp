beb3d11241f9b9232211714bb653b3391c9c7174
sroya <shekeb.roya@hsr.ch>
2017-11-25 14:45:48 +0100

    header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h1>Header</h1>;
+import Header from './Header';
 const Landingpage = () => <h1>Landingpage</h1>;
 const Dashboard = () => <h1>Dashboard</h1>;
-const FeddbackNew = () => <h1>FeddbakNew</h1>;
+const FeedbackNew = () => <h1>FeedbackNew</h1>;
+const Footer = () => <h1>Footer</h1>;
 
 const App = () => {
   return (
@@ -13,8 +14,9 @@ const App = () => {
         <div>
           <Header />
           <Route exact path="/" component={Landingpage} />
-          <Route exact path="/umfrage" component={Dashboard} />
-          <Route path="/umfrage/neue-umfrage" component={FeddbackNew} />
+          <Route exact path="/umfragen" component={Dashboard} />
+          <Route path="/umfragen/neue-umfrage" component={FeedbackNew} />
+          <Footer />
         </div>
       </BrowserRouter>
     </div>
