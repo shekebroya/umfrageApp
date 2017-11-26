4fd4c41981b3f11de8139c194c7ec8b288f0f489
sroya <shekeb.roya@hsr.ch>
2017-11-26 16:01:05 +0100

    refactor promis with async

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,12 +2,10 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 // Action Creator
-export const fetchUser = () => {
+export const fetchUser = () =>
   // ReduxThunk erkennt das es eine function ist, somit wird es automatisch aufgerufen mit dispatch (Versand)
   // Versende eine action nachdem "axios.get('/api/current_user')" action vervollständigt wurde.
-  return function(dispatch) {
-    axios
-      .get('/api/current_user') // Request (req)
-      .then(res => dispatch({ type: FETCH_USER, payload: res })); // Respond (res) erst jetzt wird die Action versendet.
+  async dispatch => {
+    const res = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: res }); // Respond (res) erst jetzt wird die Action versendet.
   };
-};
