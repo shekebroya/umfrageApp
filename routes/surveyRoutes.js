edc473ece1125e88f45d875f7fa7002e9823a173
sroya <shekeb.roya@hsr.ch>
2017-11-30 15:23:28 +0100

    sendgrid + umfrage routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-  app.post('/api/umfragen', requireLogin, (req, res) => {});
+  app.post('/api/umfragen', requireLogin, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => {
+        return { email: email.trim() };
+      }),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
