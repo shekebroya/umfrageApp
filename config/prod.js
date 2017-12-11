5589aaae4f40920d189034cd388d9a20ebdc8302
sroya <shekeb.roya@hsr.ch>
2017-12-11 21:45:19 +0100

    redirect bug

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN;
+  redirectDomain: process.env.REDIRECT_DOMAIN
 };
